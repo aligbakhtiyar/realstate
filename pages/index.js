921358e2dae9ba647d376cef2961487a2ed63650
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-02-08 10:04:57 +0530

    index commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import {Flex, Box, Text, Button} from '@chakra-ui/react';
 
+
+
+const Flex = () => (
+  <Flex flexWrap= "wrap" justifyContent = 'center' alignItems='center' m='10'>
+  {purpose}
+</Flex>
+)
+ 
 
 export default function Home() {
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>
+        Hello
+      </h1>
+      <Banner purpose= {'For Sale'} />
+      <Banner purpose= {'For Rent'} />
+    </div>
   )
 }
